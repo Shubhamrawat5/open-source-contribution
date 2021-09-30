a5484922a4e64c2e9bc450a146e7574fbcf0aea6
Shubhamrawat5 <sr856161@gmail.com>
2021-10-01 02:29:16 +0530

    add java language folder

---
+++ JAVA/prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class prime
+{
+   public static void main(String args[])
+   {		
+	int temp;
+	boolean isPrime=true;
+	Scanner scan= new Scanner(System.in);
+	System.out.println("Enter any number:");
+	int num=scan.nextInt();
+	for(int i=2;i<=num/2;i++)
+	{
+           temp=num%i;
+	   if(temp==0)
+	   {
+	      isPrime=false;
+	      break;
+	   }
+	}
+	if(isPrime)
+	   System.out.println(num + " is a Prime Number");
+	else
+	   System.out.println(num + " is not a Prime Number");
+   }
+}
