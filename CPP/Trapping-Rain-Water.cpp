fdfb3479f9de8bc5948829b3262d0a43bab5cc89
Abhishek Dhiman <49456787+abhishek-dhnma@users.noreply.github.com>
2021-10-01 21:39:53 +0530

    Add files via upload

---
+++ CPP/Trapping-Rain-Water.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void TrappingRainWater(int arr[], int n){
+
+int result = 0;
+
+    for (int i = 1; i < n-1; i++)
+    {
+        // Right
+        int right = arr[i];
+        for (int j = i+1; j < n ; j++)
+        {
+            right = max(right,arr[j]);
+        }
+        
+        // Left
+
+        int left = arr[i];
+        for (int j = 0; j < i ; j++)
+        {
+            left = max(left,arr[j]);
+        }
+
+        
+
+        result = result + (min(left,right) - arr[i]);
+
+
+    }
+    
+    cout<< result ;
+
+
+}
+
+
+int main()
+{   int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    TrappingRainWater(arr,n);    
+    return 0;
+}
