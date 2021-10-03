531671d8bf391fda59b942f36979e4536cf55253
overthinker910 <79561540+overthinker910@users.noreply.github.com>
2021-10-03 15:57:50 +0530

    Create CreditCardStrength.java

---
+++ PYTHON/CreditCardStrength.java
@@ -0,0 +1,19 @@
+//Program to P to verify whether his credit card numbers are valid or not. A valid credit card
+//from ABC Bank has the following characteristics:
+//● It must start with a 4,5 or 6 .
+//● It must contain exactly 16 digits.
+//● It must only consist of digits (0-9).
+//● It may have digits in groups of 4, separated by one hyphen ‘-’
+
+
+import re
+num= input("enter your credit card number:")
+print(len(num))
+if re.search(r'^[456]+',num) and re.search(r'[\d]',num) and bool(re.se
+arch(r'\s',num)) is False and bool (re.search(r'[az]',num)) is False and(bool(re.search(r'-
+',num))is True and len(num)==19):
+  print("The entered credit card number is valid")
+else:
+  print("The entered credit card number is invalid")
+
+
