f78a713624772e0fd43153cd778a85478bac6fcd
zeenayouhan <zinayouhan33@gmail.com>
2021-10-03 19:38:09 +0530

    added helloworld

---
+++ C/helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	  
+    printf("Hello, World!");
+    printf("\n%s",s); 
+    return 0;
+}
