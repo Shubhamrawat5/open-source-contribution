1253f63921f641c3661fac5cae0b06797d9680aa
Tanmay Mathur <74481466+RYzen-009@users.noreply.github.com>
2021-10-07 08:29:17 +0530

    check for palindrome

---
+++ CPP/GfgDsa/Introduction/palindrome.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+bool isPalRec(char str[], int s, int e)
+{
+    if (s == e)
+        return true;
+    if (str[s] != str[e])
+        return false;
+     if (s < e + 1)
+        return isPalRec(str, s + 1, e - 1);
+    return true;
+}
+
+bool isPalindrome(char str[])
+{
+    int n = strlen(str);
+
+    if (n == 0)
+      return true;
+
+return isPalRec(str, 0, n - 1);
+}
+
+
+int main()
+{
+    char str[];
+    for(char ch=0;)
+    if (isPalindrome(str))
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
+
+
+*/
