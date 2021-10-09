1f49aa6a9ea4c54a14ea079ac352e1eff7bdcf10
unknown <sibashishbiswas349@gmail.com>
2021-10-09 08:50:42 +0530

    New problems in java folder

---
+++ JAVA/rearrageArrayAlternative.java
@@ -0,0 +1,73 @@
+// https://practice.geeksforgeeks.org/problems/-rearrange-array-alternately-1587115620/1
+
+
+import java.io.*;
+
+class rearrangeArrayAlternative {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    int[] temp= new int[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        temp[i] = Integer.parseInt(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(temp, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(temp[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+
+class Solution{
+    
+    public static void rearrange(int arr[], int n){
+        
+        // Your code here
+        int temp[] = arr.clone();
+        int small = 0, large = n - 1;
+        boolean flag = true;
+ 
+        for (int i = 0; i < n; i++) {
+            if (flag)
+                arr[i] = temp[large--];
+            else
+                arr[i] = temp[small++];
+ 
+            flag = !flag;
+        }
+    }
+    
+}
+
+
