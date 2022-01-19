c383fbce0227ac012a7ca26108cb402a2a2c2413
Krish Chopra <77012237+KrishChopra22@users.noreply.github.com>
2022-01-20 00:01:03 +0530

    Calculator Program added by Krish

---
+++ CPP/Calculator_Program.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+float a, b, res;
+	char choice, ch;
+	do
+	{
+		cout<<"1.Addition\n";
+		cout<<"2.Subtraction\n";
+		cout<<"3.Multiplication\n";
+		cout<<"4.Division\n";
+		cout<<"5.Exit\n\n";
+		cout<<"Enter Your Choice : ";
+		cin>>choice;
+		switch(choice)
+		{
+			case '1' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a+b;
+				cout<<"Result = "<<res;
+				break;
+			case '2' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a-b;
+				cout<<"Result = "<<res;
+				break;
+			case '3' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a*b;
+				cout<<"Result = "<<res;
+				break;
+			case '4' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a/b;
+				cout<<"Result = "<<res;
+				break;
+			case '5' : exit(0);
+				break;
+			default : cout<<"Wrong Choice..!!";
+				break;
+		}
+		cout<<"\n------------------------------------\n";
+	}while(choice!=5 && choice!=getchar());
+}
