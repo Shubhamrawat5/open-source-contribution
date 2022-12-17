764857243f436c5f440c37b09e0ddd439f21a8fb
Karim129 <karimashraf851@gmail.com>
2022-12-17 21:46:14 +0200

    optimal hyperparameter adjustment

--- PYTHON/simple_model_k_neighbors/model_to_predict.py
+++ PYTHON/simple_model_k_neighbors/model_to_predict.py
@@ -1,24 +1,29 @@
-# get iris data from sklearn
-import sklearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # load iris data
 iris = load_iris()
 
 
 # split data
-X_train, X_test, y_train, y_test = train_test_split(
-    iris.data, iris.target, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=0)
+# Search for the optimal hyperparameter 
+N_range = list(range(1, 18))
+knn = KNeighborsClassifier(n_jobs=-1)
+param_grid = {'n_neighbors':N_range ,'weights':["uniform", "distance"]}
 
-# train model
-knn = KNeighborsClassifier(n_neighbors=1)
-knn.fit(X_train, y_train)
+grid = GridSearchCV(knn, param_grid, cv=10, scoring='accuracy', return_train_score=False)
+grid.fit(X_train, y_train)
 
-# predict
-y_pred = knn.predict(X_test)
 
-# evaluate
-print(accuracy_score(y_test, y_pred))
+print('This is the score of the training : ' , grid.best_score_)
+print('Best parameters is  ' , grid.best_params_)
+print('Best estimators : ' , grid.best_estimator_)
+#Evaluation and calculate the model accuracy
+grid.best_estimator_.fit(X_train,y_train)
+out=grid.score(X_test,y_test)
+print(f"This is model accuracy {out}")
