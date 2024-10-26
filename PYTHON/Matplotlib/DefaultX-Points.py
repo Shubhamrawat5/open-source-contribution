388c05d73c22e472fec31684097f828db24d6c49
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-10-26 10:29:39 +0530

    Create DefaultX-Points.py

---
+++ PYTHON/Matplotlib/DefaultX-Points.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# if we do not specify the pont on the x-axis they will get the default values 0,1,2,3,..etc
+
+yaxis=np.array([3,1,17,10,5,7])
+plt.plot(yaxis)
+plt.show()
