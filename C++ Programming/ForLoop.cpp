19186eeb93a0784cab71504f7adee39c8ce6321a
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-10-29 17:59:57 +0530

    Added Code
    Added Code

---
+++ C++ Programming/ForLoop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int day;
+	cout<<"Enter the day of the week:";
+	cin>>day;
+	for(int i=0;i<=day;i++)
+	{
+		cout<<"day:"<<i<<endl;
+	}
+	return 0;
+}
