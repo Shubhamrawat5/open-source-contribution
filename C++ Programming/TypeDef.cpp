8997f8852155d03bda9bdeae09a4aeb98adc33e4
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-11-07 10:06:36 +0530

    Added Code

---
+++ C++ Programming/TypeDef.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+typedef struct food
+{
+    int quantity;
+    char name;
+    float price;
+}ep;
+int main()
+
+{
+    ep Lunch;
+    ep Breakfast;
+    cout<<"Enter the quantity of food : ";
+    cin>>Lunch.quantity;
+    cout<<"Enter the Starting letter of food : ";
+    cin>>Lunch.name;
+    cout<<"Enter the prince of food : ";
+    cin>>Lunch.price;
+    cout<<endl<<"Quantity of Lunch : "<<Lunch.quantity<<endl;
+    cout<<"Starting letter of Lunch : "<<Lunch.name<<endl;
+    cout<<"Price of Lunch: "<<Lunch.price<<endl;
+
+    cout<<"Enter the quantity of food : ";
+    cin>>Breakfast.quantity;
+    cout<<"Enter the Starting letter of food : ";
+    cin>>Breakfast.name;
+    cout<<"Enter the prince of food : ";
+    cin>>Breakfast.price;
+    cout<<endl<<"Quantity of Breakfast : "<<Breakfast.quantity<<endl;
+    cout<<"Starting letter of Breakfast : "<<Breakfast.name<<endl;
+    cout<<"Price of Breakfast : "<<Breakfast.price<<endl;
+    
+    return 0;
+}
