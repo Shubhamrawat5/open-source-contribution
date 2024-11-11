bff1c51d922f0143ccbe31703ec67ec0b9ad248a
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-11-11 18:52:55 +0530

    Added Code

---
+++ JAVA/ZeroToTenWhile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ZeroToTenWhile 
+{
+    public static void main(String[] args)
+    {
+        int i = 1;
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }   
+} 
