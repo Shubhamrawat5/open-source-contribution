ade90087a5f8c4c533fb6639fad74bd7b387ac76
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-11-11 18:53:50 +0530

    Added Code

---
+++ JAVA/ZeroToTenForLoop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ZeroToTenForLoop 
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }    
+}
