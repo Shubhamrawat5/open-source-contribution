101cbfc3a7eb4c89868e7de65fa6690630e8fd13
SUDARSHAN SANTAJI JADHAV <145355498+SUDARSHAN2171@users.noreply.github.com>
2024-11-12 11:42:18 +0530

    Added Code

---
+++ JAVA/Output.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Output {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println(name);
+    }
+}
